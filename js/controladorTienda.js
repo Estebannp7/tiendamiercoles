9be24b5b35b02d34e240060d9cf5b94ab66888f4
203 <203@academicos.cesde.edu.co>
2022-02-16 09:04:27 -0600

    Se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,33 @@
+//ENTRADAS DEL PROBLEMA
+
+
+//ENTRADAS == VARIABLES == CONSTANTES 
+
+let nombreProducto = "Ghost"
+let precioProducto = 400000
+let pesoProducto= 18.5
+let tieneDescuento = true
+let descripcionProducto = "Vehiculo "
+let telefonoVendendor= "322599231"
+
+const NOMBRE_CLIENTE = "Esteban"
+
+
+//ARREGLOS UNIDIMENSIONALES
+//sirven para almacenar varios elementos en una sola variable
+
+let productos= ["Ghost","Phantom","Wraith"]
+let productos2= Array("Ghost","Phantom","Wraith")
+
+
+
+
+
+
+//SALIDAS
+console.log("hablame")  
+console.log(nombreProducto)     
+console.log("su nombre es: "+NOMBRE_CLIENTE+" y su producto es: "+nombreProducto)
+console.log(`su nombre es : ${NOMBRE_CLIENTE} y su producto es ${nombreProducto}`)
+console.log(productos)
+console.log(productos2[2])
